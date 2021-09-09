09d755fd349c77535e962fdca625aa27af0da36a
albator1983 <kim13kara@gmail.com>
2021-09-09 20:05:37 +0200

    cv finni

--- cv-react2/src/App.js
+++ cv-react2/src/App.js
@@ -1,33 +1,62 @@
-import './App.css';
-import User from './components/User';
-import Skills from './components/Skills';
+import "./App.css"
+import User from "./components/User"
+import Skills from "./components/Skills"
+import Profil from "./components/Profil"
+import Cursus from "./components/FormationsExperiences"
+import DarkMode from "./components/DarkMode"
+import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf"
+import { Preview, print } from "react-html2pdf"
 
 function App() {
+  const handleGenerateCv = () => {
+    let cvTemplate = document.getElementById("cv-print")
+    cvTemplate.setAttribute("style", "width:210mm !important")
+    cvTemplate.classList.add("cv-print")
+    document.body.classList.remove("dark")
+    setTimeout(() => {
+      print("cv", "cv-print")
+      cvTemplate.setAttribute("style", "width:100% !important")
+      cvTemplate.classList.remove("cv-print")
+    }, 300)
+  }
   return (
-    <div className="App">
-     <div className="grid-container">
-       <div className="sidebar">
-         <User />
-         <Skills />
-       </div>
-       <div className="main">main</div>
-       </div>
-       {/* react, material ui, react-html2pdf */}
-       {/* variable css */}
-       {/* colonne gauche */}
-       {/* details personnel */}
-       {/* competences-skills */}
-       {/* interest */}
+    <Preview id={"cv-print"} class="cv-print">
+      <div className="App">
+        <div className="grid__container">
+          <div className="sidebar">
+            <div className="actions">
+              <DarkMode />
+              <button onClick={handleGenerateCv}>
+                <PictureAsPdfIcon />
+              </button>
+            </div>
+            <User />
+            <Skills />
+          </div>
+          <div className="main">
+            <Profil />
+            <Cursus />
+          </div>
+        </div>
+        {/* react, material ui, react-html2pdf */}
+        {/* Installation, Nettoyage & paramétrage */}
+        {/* Variables CSS */}
+        {/* Colonne de gauche * /} 
 
-       {/* colonne de droite */}
-       {/* profil */}
-       {/* formation */}
-       {/* experience */}
+      {/* Détails personnels - User */}
+        {/* Compétences - skills */}
+        {/* Langues - skills */}
+        {/* Centres d'intérêts - interests */}
+        {/* Colonne de droite * /} 
 
-       {/* light/dark mode */}
-       {/* generation de pdf */}
-    </div>
-  );
+      {/* Profil - profil */}
+        {/* Formation  - education */}
+        {/* Expériences professionelles - experiences */}
+        {/* Ligth & Dark mode */}
+        {/* Export PDF (react-html2pdf) */}
+      </div>
+    </Preview>
+  )
 }
 
-export default App;
+export default App
---
+++ cv-react2/src/components/Profil.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profil = () => {
+    return (
+        <div className="profil">
+            <h2 className="h2">Profil</h2>
+            <p>
+                Passion is machines!
+            </p>
+        </div>
+    );
+};
+
+export default Profil;
